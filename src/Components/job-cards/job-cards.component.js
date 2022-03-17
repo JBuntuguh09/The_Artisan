83fb00adb23442a56be9f79acf74aa6a42714ba3
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-03-17 23:12:15 +0000

    move all local data to the data.js file

--- src/Components/job-cards/job-cards.component.js
+++ src/Components/job-cards/job-cards.component.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from "react-bootstrap";
 import HomeCarousel from "../carousel/carousel.component";
 import CustomButton from "../Resources/custom-button/custom-button.component";
+import { caroArray } from "../Resources/data";
 
 export class JobCards extends React.Component {
     constructor(props) {
@@ -16,29 +17,7 @@ export class JobCards extends React.Component {
 
     }
 
-    caroArray = [
-        {
-            title: "Plumbers",
-            imageUrl:
-                "https://media.istockphoto.com/photos/black-male-plumber-at-work-picture-id1348934184?b=1&k=20&m=1348934184&s=170667a&w=0&h=Y6_76kemqf4aSEPwM5xEw9pxbyEM9QMTMVouRiJM72M=",
-            caption: "We have the best plumbers here",
-            alt: "first slide"
-        },
-        {
-            title: "Capenters",
-            imageUrl:
-                "https://media.istockphoto.com/photos/plumber-at-work-in-a-bathroom-plumbing-repair-service-assemble-and-picture-id918319088?b=1&k=20&m=918319088&s=170667a&w=0&h=-YgoN2GgVmYrEMFLz_qsb8LZvIWmjJHaKrIjKrTiQ2U=",
-            caption: "We have the best Capenters here",
-            alt: "second slide"
-        },
-        {
-            title: "Brick Layer/Mason",
-            imageUrl:
-                "https://media.istockphoto.com/photos/construction-workers-building-concrete-block-wall-picture-id175205008?b=1&k=20&m=175205008&s=170667a&w=0&h=YCFU_mz9ZNG1z2fW-HGYhH3utfPVYNB4BIlqe18fODA=",
-            caption: "We have the best masons/brick layers here",
-            alt: "third slide"
-        },
-    ];
+    caroArray = caroArray
 
     // componentDidMount() {
     //     fetch("https://jsonplaceholder.typicode.com/photos")
@@ -68,7 +47,6 @@ export class JobCards extends React.Component {
                         </Container>
                     )
                 })}
-
             </>
         );
     }
